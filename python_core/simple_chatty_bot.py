061b68495a755428ebfa4a94acbbc5e0fc84deb4
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2023-03-04 18:13:00 -0300

    wip

--- python_core/simple_chatty_bot.py
+++ python_core/simple_chatty_bot.py
@@ -3,3 +3,7 @@ birth_year = 2020
 
 print(f"Hello! My name is {bot_name}.")
 print(f"I was created in {birth_year}.")
+
+print("Please, remind me your name.")
+your_name = input()
+print(f"What a great name you have, {your_name}!")
