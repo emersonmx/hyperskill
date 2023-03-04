f30f74392dc7e11008070ca48d8e3f793457e1fc
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2023-03-04 18:51:14 -0300

    wip

--- python_core/last_pencil.py
+++ python_core/last_pencil.py
@@ -1,2 +1,9 @@
-print("||||||||")
-print("Your turn!")
+print("How many pencils would you like to use:")
+pencils = int(input())
+
+players = ["John", "Jack"]
+print(f"Who will be the first ({', '.join(players)}):")
+player = input()
+
+print("|" * pencils)
+print(f"{player} is going first!")
