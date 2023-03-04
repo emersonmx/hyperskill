d23543822eebbd12f9d2b4961e4ff93b552f4c5b
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2023-03-04 18:15:29 -0300

    wip

--- python_core/simple_chatty_bot.py
+++ python_core/simple_chatty_bot.py
@@ -7,3 +7,11 @@ print(f"I was created in {birth_year}.")
 print("Please, remind me your name.")
 your_name = input()
 print(f"What a great name you have, {your_name}!")
+
+print("Let me guess your age.")
+print("Enter remainders of dividing your age by 3, 5 and 7.")
+remainder3 = float(input())
+remainder5 = float(input())
+remainder7 = float(input())
+your_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+print(f"Your age is {your_age}; that's a good time to start programming!")
