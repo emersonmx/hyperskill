7d0136ae3100305c70a0f99a0ad7174ae02e1772
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2023-03-05 17:33:32 -0300

    wip

--- python_core/last_pencil.py
+++ python_core/last_pencil.py
@@ -1,52 +1,77 @@
-print("How many pencils would you like to use:")
-pencils = 0
-while True:  # TODO: Create function
-    pencils = input().strip()  # TODO: Create clean_input()
-    # TODO: Use int() and catch exception
-    is_negative = pencils.startswith("-") and pencils[1:].isdigit()
-    is_zero = pencils.isdigit() and pencils == "0"
-    if is_negative or is_zero:
-        print("The number of pencils should be positive")
-    elif pencils.isdigit():
-        pencils = int(pencils)
-        break
-    else:
-        print("The number of pencils should be numeric")
+PLAYERS = ["John", "Jack"]
 
-players = ["John", "Jack"]
-player_index = 0
-print(f"Who will be the first ({', '.join(players)}):")
-while True:  # TODO: Create function
-    player_name = input()
-    if player_name in players:
-        player_index = players.index(player_name)
-        break
-    else:
+
+def get_pencil_amount():
+    while True:
+        try:
+            pencils = int(input())
+        except ValueError:
+            print("The number of pencils should be numeric")
+            continue
+
+        if pencils > 0:
+            return pencils
+
+        print("The number of pencils should be positive")
+
+
+def get_first_player():
+    while True:
+        name = input()
+        if name in PLAYERS:
+            return name
+
         print("Choose between 'John' and 'Jack'")
 
-while pencils > 0:  # TODO: Create function
-    print("|" * pencils)
-    player_name = players[player_index]
-    print(f"{player_name}'s turn:")
-    player_input = 0
+
+def get_player_index(name):
+    return PLAYERS.index(name)
+
+
+def get_player_name(index):
+    return PLAYERS[index]
+
+
+def get_next_player(name):
+    index = get_player_index(name)
+    return get_player_name((index + 1) % len(PLAYERS))
+
+
+def get_player_input(pencils):
     while True:
-        player_input = input()
-        if not player_input.isdigit():
+        try:
+            player_input = int(input())
+        except ValueError:
             print("Possible values: '1', '2' or '3'")
             continue
 
-        player_input = int(player_input)
         in_range = 1 <= player_input <= 3
         if in_range:
             if player_input <= pencils:
-                break
+                return player_input
             else:
                 print("Too many pencils were taken")
         else:
             print("Possible values: '1', '2' or '3'")
 
-    pencils -= player_input
-    player_index = (player_index + 1) % len(players)
 
-player_name = players[player_index]
-print(f"{player_name} won!")
+def main() -> int:
+    print("How many pencils would you like to use:")
+    pencils = get_pencil_amount()
+
+    print(f"Who will be the first ({', '.join(PLAYERS)}):")
+    player = get_first_player()
+
+    while pencils > 0:
+        print("|" * pencils)
+        print(f"{player}'s turn:")
+        pencils -= get_player_input(pencils)
+        player = get_next_player(player)
+
+    print(f"{player} won!")
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
