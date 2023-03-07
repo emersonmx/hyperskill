e10e94b9b5d2ae968178050268af4f5a81f97ed1
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2023-03-07 20:53:04 -0300

    wip

--- python_core/rock_paper_scissors.py
+++ python_core/rock_paper_scissors.py
@@ -1,15 +1,21 @@
+import random
+
+
 def main():
+    inputs = ["rock", "paper", "scissors"]
     playerInput = input()
+    computerInput = random.choice(inputs)
 
-    result = ""
-    if playerInput == "paper":
-        result = "scissors"
-    elif playerInput == "scissors":
-        result = "rock"
-    elif playerInput == "rock":
-        result = "paper"
+    i = inputs.index(playerInput)
+    j = i + 1
+    check_list = inputs[j:] + inputs[:i]
 
-    print(f"Sorry, but the computer chose {result}")
+    if playerInput == computerInput:
+        print(f"There is a draw ({playerInput})")
+    elif check_list.index(computerInput) == 0:
+        print(f"Sorry, but the computer chose {computerInput}")
+    elif check_list.index(computerInput) == 1:
+        print(f"Well done. The computer chose {computerInput} and failed")
 
 
 if __name__ == "__main__":
