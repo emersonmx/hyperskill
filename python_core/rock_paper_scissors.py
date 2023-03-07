033fa6aaaa4658cc8f65e168bdcb97534717bd68
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2023-03-07 20:58:45 -0300

    wip

--- python_core/rock_paper_scissors.py
+++ python_core/rock_paper_scissors.py
@@ -8,13 +8,13 @@ def main():
 
     i = inputs.index(playerInput)
     j = i + 1
-    check_list = inputs[j:] + inputs[:i]
+    checks = inputs[j:] + inputs[:i]
 
     if playerInput == computerInput:
         print(f"There is a draw ({playerInput})")
-    elif check_list.index(computerInput) == 0:
+    elif checks.index(computerInput) == 0:
         print(f"Sorry, but the computer chose {computerInput}")
-    elif check_list.index(computerInput) == 1:
+    elif checks.index(computerInput) == 1:
         print(f"Well done. The computer chose {computerInput} and failed")
 
 
