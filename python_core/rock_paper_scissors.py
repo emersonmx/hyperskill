333bee03f702cb98d209411bfbc1fbe0051714d8
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2023-03-07 19:41:50 -0300

    wip

--- python_core/rock_paper_scissors.py
+++ python_core/rock_paper_scissors.py
@@ -1,1 +1,16 @@
-print("Hello World")
+def main():
+    playerInput = input()
+
+    result = ""
+    if playerInput == "paper":
+        result = "scissors"
+    elif playerInput == "scissors":
+        result = "rock"
+    elif playerInput == "rock":
+        result = "paper"
+
+    print(f"Sorry, but the computer chose {result}")
+
+
+if __name__ == "__main__":
+    main()
