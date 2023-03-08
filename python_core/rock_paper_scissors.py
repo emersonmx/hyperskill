74ee81570334d7a18c509da1af6d23ed5b749c3e
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2023-03-07 21:09:19 -0300

    wip

--- python_core/rock_paper_scissors.py
+++ python_core/rock_paper_scissors.py
@@ -1,21 +1,36 @@
 import random
 
 
+def clean_input():
+    return input().strip()
+
+
 def main():
     inputs = ["rock", "paper", "scissors"]
-    playerInput = input()
-    computerInput = random.choice(inputs)
+    options = inputs + ["!exit"]
 
-    i = inputs.index(playerInput)
-    j = i + 1
-    checks = inputs[j:] + inputs[:i]
+    while True:
+        playerInput = clean_input()
+        if playerInput not in options:
+            print("Invalid input")
+            continue
+        if playerInput == "!exit":
+            break
 
-    if playerInput == computerInput:
-        print(f"There is a draw ({playerInput})")
-    elif checks.index(computerInput) == 0:
-        print(f"Sorry, but the computer chose {computerInput}")
-    elif checks.index(computerInput) == 1:
-        print(f"Well done. The computer chose {computerInput} and failed")
+        computerInput = random.choice(inputs)
+
+        i = inputs.index(playerInput)
+        j = i + 1
+        checks = inputs[j:] + inputs[:i]
+
+        if playerInput == computerInput:
+            print(f"There is a draw ({playerInput})")
+        elif checks.index(computerInput) == 0:
+            print(f"Sorry, but the computer chose {computerInput}")
+        elif checks.index(computerInput) == 1:
+            print(f"Well done. The computer chose {computerInput} and failed")
+
+    print("Bye!")
 
 
 if __name__ == "__main__":
