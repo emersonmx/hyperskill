beaa3e947304dfc43fd2b82f6bb1f3556fed18b6
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2023-03-12 15:12:13 -0300

    wip

--- python_core/rock_paper_scissors.py
+++ python_core/rock_paper_scissors.py
@@ -5,30 +5,67 @@ def clean_input():
     return input().strip()
 
 
+def get_result(inputs, player1, player2):
+    i = inputs.index(player1)
+    j = i + 1
+    checks = inputs[j:] + inputs[:i]
+    if player1 == player2:
+        return None
+    elif checks.index(player2) == 0:
+        return player2
+    elif checks.index(player2) == 1:
+        return player1
+
+
+def fetch_score(player_name):
+    with open("rating.txt", "r") as f:
+        for line in f:
+            name, score = line.strip().split()
+            if player_name == name:
+                return int(score)
+    return 0
+
+
+def calculate_score(player1, player2, result):
+    if result == player1:
+        return 100
+    elif result == player2:
+        return 0
+    else:
+        return 50
+
+
 def main():
     inputs = ["rock", "paper", "scissors"]
-    options = inputs + ["!exit"]
+    options = inputs + ["!rating", "!exit"]
+
+    print("Enter your name:")
+    player_name = clean_input()
+    print(f"Hello, {player_name}")
+    score = fetch_score(player_name)
+    print(score)
 
     while True:
-        playerInput = clean_input()
-        if playerInput not in options:
+        player1 = clean_input()
+        if player1 not in options:
             print("Invalid input")
             continue
-        if playerInput == "!exit":
+        if player1 == "!rating":
+            print(f"Your rating: {score}")
+            continue
+        if player1 == "!exit":
             break
 
-        computerInput = random.choice(inputs)
+        player2 = random.choice(inputs)
 
-        i = inputs.index(playerInput)
-        j = i + 1
-        checks = inputs[j:] + inputs[:i]
-
-        if playerInput == computerInput:
-            print(f"There is a draw ({playerInput})")
-        elif checks.index(computerInput) == 0:
-            print(f"Sorry, but the computer chose {computerInput}")
-        elif checks.index(computerInput) == 1:
-            print(f"Well done. The computer chose {computerInput} and failed")
+        result = get_result(inputs, player1, player2)
+        score += calculate_score(player1, player2, result)
+        if result == player1:
+            print(f"Well done. The computer chose {player2} and failed")
+        elif result == player2:
+            print(f"Sorry, but the computer chose {player2}")
+        else:
+            print(f"There is a draw ({player1})")
 
     print("Bye!")
 
